b108b83ccc964e0fe3b43e3918a37de50fd43e1b
slimnation23 <slimnation2@gmail.com>
2020-07-01 15:42:52 +0300

    header

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
-let dropdownMenu = document.getElementById('dropdown-menu');
-let dropdownBtn = document.getElementById('dropdown-btn');
-let menu = document.getElementById('menu');
-let menuBtn = document.getElementById('menu-btn');
+let dropdownMenu = document.getElementById("dropdown-menu");
+let dropdownBtn = document.getElementById("dropdown-btn");
+let menu = document.getElementById("menu");
+let menuBtn = document.getElementById("menu-btn");
 
-menuBtn.onclick = function() {
-    menu.classList.toggle('active');
+menuBtn.onclick = function () {
+  menu.classList.toggle("active");
 };
 
-dropdownBtn.onclick = function() {
-    dropdownMenu.classList.toggle('drop-active');
+dropdownBtn.onclick = function () {
+  dropdownMenu.classList.toggle("drop-active");
 };
 
-$('#online-slider').slick({
+$("#online-slider").slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  variableWidth: true,
+  arrows: false,
+});
+
+$("#qualified-slider").slick({
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
-    variableWidth: true
+    autoplaySpeed: 2100,
+    variableWidth: true,
+    arrows: false,
   });
