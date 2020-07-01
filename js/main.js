6c758c5e6ca4c73a178d9dcdafa5bfd467dc8ae6
slimnation23 <slimnation2@gmail.com>
2020-07-01 13:42:45 +0300

    slider1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let dropdownMenu = document.getElementById('dropdown-menu');
+let dropdownBtn = document.getElementById('dropdown-btn');
+let menu = document.getElementById('menu');
+let menuBtn = document.getElementById('menu-btn');
+
+menuBtn.onclick = function() {
+    menu.classList.toggle('active');
+};
+
+dropdownBtn.onclick = function() {
+    dropdownMenu.classList.toggle('drop-active');
+};
+
+$('#online-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true
+  });
